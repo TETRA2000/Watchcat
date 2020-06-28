225c527ff366c3abaeb1276756ac06e95a03109c
TETRA2000 <sagittariusm25@gmail.com>
2020-06-28 13:17:10 +0900

    Initial Commit

---
+++ Shared/WatchcatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchcatApp.swift
+//  Shared
+//
+//  Created by Takahiko Inayama on 2020/06/28.
+//
+
+import SwiftUI
+
+@main
+struct WatchcatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
